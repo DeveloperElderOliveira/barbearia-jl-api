091dfadf1a470218d8fbe911828fff016627fd79
DeveloperElderOliveira <e.docarmooliveira@hotmail.com>
2022-08-04 17:11:54 -0300

    ;

--- app/Http/Controllers/SchedulesController.php
+++ app/Http/Controllers/SchedulesController.php
@@ -51,7 +51,8 @@ class SchedulesController extends Controller
     
     public function loadHorarios($dia)
     {   
-        $escala_horarios_disponiveis = ["08:00 - 09:00",
+        $escala_horarios_disponiveis = [
+        " ","08:00 - 09:00",
         "09:00 - 10:00","10:00 - 11:00",
         "11:00 - 12:00","12:00 - 13:00",
         "13:00 - 14:00","14:00 - 15:00", 
