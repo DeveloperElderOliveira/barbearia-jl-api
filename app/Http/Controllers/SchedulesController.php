02d1a1e66dc925beebbdcce89d59f35336280451
DeveloperElderOliveira <e.docarmooliveira@hotmail.com>
2022-08-04 17:30:07 -0300

    ;

--- app/Http/Controllers/SchedulesController.php
+++ app/Http/Controllers/SchedulesController.php
@@ -65,7 +65,8 @@ class SchedulesController extends Controller
         foreach($horarios_agendados as $hora_agendada){
             $pos = array_search($hora_agendada->horario,$escala_horarios_disponiveis);
             if($pos != false){
-                $disp[] = $hora_agendada->horario;
+                $disp[] = pos;
+                unset($escala_horarios_disponiveis[$pos]);
             }
                
         }
