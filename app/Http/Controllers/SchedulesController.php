831f9701e47c9065cba61321365a8106cee1cab1
DeveloperElderOliveira <e.docarmooliveira@hotmail.com>
2022-08-04 08:43:39 -0300

    ;

--- app/Http/Controllers/SchedulesController.php
+++ app/Http/Controllers/SchedulesController.php
@@ -30,7 +30,7 @@ class SchedulesController extends Controller
         if(!$schedules = $this->schedule->with('employee','user','services','agendamento_dia_horario')->where('user_id',$user['id'])->orderBy('scheduling_date')->get())
              return response()->json(['error' => 'schedules not found.']);
 
-             dd($schedules->services);
+             dd($schedules);
 
         return response()->json($schedules);
     } 
